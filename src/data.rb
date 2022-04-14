965b39dc8aad45a5fe4ec087355401a4b6782b5c
karlkemistersheppard <10342@coderacademy.edu.au>
2022-04-14 13:57:01 +1000

    updated arrays objects "" with quote marks

--- src/data.rb
+++ src/data.rb
@@ -1,13 +1,12 @@
 require_relative("./$global_unicodes")
 require_relative("./$global_var.rb")
-require_relative("./class_character.rb")
+require_relative"./class_character.rb"
 require_relative("./class_gender.rb")
 require_relative("./class_tools.rb")
 require_relative("./class_potion.rb")
 
-
-def data
     #Characters Objects
+def data_character
     witcher = Character.new("witcher","strength", "1200")
     human = Character.new("human","vision", "500")
     blood_elvf = Character.new("blood_elf","extra life", "600")
@@ -17,28 +16,32 @@ def data
     spartan = Character.new("spartan","speed", "100")
     iron_dwarf = Character.new("iron dwarf","deflective shield", "300")
     kree_warrior = Character.new("kree warrior","strength", "200")
-
-    #Gender Objects
+    character = ["witcher", "human", "blood_elvf", "night_crawler", "warthog", "soul_reaper", "spartan", "iron_dwarf", "kree_warrior"]
+end
+    
+#Gender Objects
+def data_gender    
     male = Gender.new("male")
     female = Gender.new("female")
-
+    gender = ["male", "female"]
+end
     #Tool Objects
-
+def data_tools
     infinity_gauntlet = Tools.new("Infinity Gauntlet","2000","2000")
     darkhold_blade = Tools.new("Dark Hold","1100","400")
     mjolnir = Tools.new("Mjolnir","900","40")
     gold_fist = Tools.new("Gold Fist","300","90")
     liquid_sword = Tools.new("Liquid Sword","200","50")
-    no_tools = Tools.new("Rock","10","30")
-
+    nothing = Tools.new("rock","10","30")
+    tools = ["infinity_gauntlet","darkhold_blade", "mjolnir", "gold_fist", "liquid_sword", "nothing"]
+end
 
     #Potion(name, description) "You have just drunk a potion called #{@name} which has given you the ability to #{@damage}"
-
+def data_potion
     wolf_blood = Potion.new("wolf blood","survive 1000 cuts from the dragon")
     vital_essence = Potion.new("vital essence","take less damage from the dragon")
     oort_brew = Potion.new("oort brew","teleport once")
     soul_dealer = Potion.new("soul dealer","take a life from a soul near by")
     fire_breath = Potion.new("fire breather","breath fire")
-
+    potion = ["wolf_blood", "vital_essence", "oort_brew", "soul_dealer", "fire_breath"]
 end
-
