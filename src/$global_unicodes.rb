d65de4f23cd5ac52e339866f258c3de83b61e88c
karlkemistersheppard <10342@coderacademy.edu.au>
2022-04-15 09:28:47 +1000

    added various unicodes

--- src/$global_unicodes.rb
+++ src/$global_unicodes.rb
@@ -18,3 +18,43 @@ TEXT
 $witcher_emoji = <<TEXT
 \u{1F9D9}
 TEXT
+
+$sword_emoji = <<TEXT
+\u{1F5E1}
+TEXT
+
+$character_emoji = <<TEXT
+\u{1F468}
+TEXT
+
+$death_emoji = <<TEXT
+\u{1F480}
+TEXT
+
+$eyes_emoji = <<TEXT
+\u{1F441}
+TEXT
+
+$gender_emoji = <<TEXT
+\u{26A5}
+TEXT
+
+$key_emoji = <<TEXT
+\u{1F5DD}
+TEXT
+
+$door_emoji = <<TEXT
+\u{1F6AA}
+TEXT
+
+$fire_emoji = <<TEXT
+\u{1F525}
+TEXT
+
+$gear_emoji = <<TEXT
+\u{2699}
+TEXT
+
+$treasure_emoji = <<TEXT
+\u{2728}\u{1F451}\u{1F48E}\u{2728}
+TEXT
