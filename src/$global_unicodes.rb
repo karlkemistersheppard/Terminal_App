d99ff8a9c948ff0f00740f74223cd7d68f099dd3
karlkemistersheppard <10342@coderacademy.edu.au>
2022-04-20 00:48:47 +1000

    Updated unicode list

--- src/$global_unicodes.rb
+++ src/$global_unicodes.rb
@@ -64,3 +64,35 @@ $copyright_emoji = <<TEXT
 \u{A9}
 TEXT
 
+
+$moon = <<TEXT
+\u{1F319}
+TEXT
+
+$tree = <<TEXT
+\u{1F332}
+TEXT
+
+$mountain_cliff = <<TEXT
+\u{26F0}
+TEXT
+
+
+$beverage = <<TEXT
+\u{1F37A}
+TEXT
+
+$wine = <<TEXT
+\u{1F377}
+TEXT
+
+
+$water = <<TEXT
+\u{1F95B}
+TEXT
+
+$fear = <<TEXT
+\u{1F631}
+TEXT
+
+
