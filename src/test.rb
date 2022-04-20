539d51f71826f5e829fef3694533a5ef4ca8010e
karlkemistersheppard <10342@coderacademy.edu.au>
2022-04-21 08:43:41 +1000

    Error handling user input + directional tty prompt

--- src/test.rb
+++ src/test.rb
@@ -1,20 +1,20 @@
 require 'colorize'
 
-# puts "So I can tell your fellow citizens about your adventure, please enter a unique character name." + " Be Creative".green
-# $character_name = STDIN.gets.chomp.magenta
-# begin
-#   name = String($character_name)
-#   # your code
-# rescue ArgumentError
-#   puts "Name must be a string"
+# $user_choice1 = ""
+# while true
+#     puts "Choose your path #{$character_name}" + " (left/right)".green
+#     $user_choice1 = STDIN.gets.chomp
+#     $user_choice1.empty? ? (puts "Direction Required") : break
 # end
+puts "So I can tell your fellow citizens about your adventure, please enter a unique character name." + " Be Creative".green
+$character_name = ""
+while true
+    puts "Please enter your characters name?"
+    $character_name = STDIN.gets.chomp.magenta
+    $character_name.empty? ? (puts "Character name required") : break
+end
 
-
-# puts "Ender in your character name"
-# user_input = STDIN.gets.chomp.magenta
-# begin
-#   age = Integer(user_input)
-#   # your code
-# rescue ArgumentError
-#   puts "Age must be an integer"
-# end
+require "tty-prompt"
+$prompt = TTY::Prompt.new
+choice = %w(left right)
+$user_choice1 = $prompt.select("Choose your path #{$character_name}", choice)
