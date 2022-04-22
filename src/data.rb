80b60d8da1afa04129349f7b2b4954d49a13ae31
karlkemistersheppard <10342@coderacademy.edu.au>
2022-04-23 00:00:22 +1000

    Deleted class require rel- (no longer needed)

--- src/data.rb
+++ src/data.rb
@@ -1,7 +1,3 @@
 require_relative("./$global_unicodes")
 require_relative("./$global_var.rb")
-require_relative"./class_character.rb"
-require_relative("./class_gender.rb")
-require_relative("./class_tools.rb")
-require_relative("./class_potion.rb")
 require_relative("./methods.rb")
